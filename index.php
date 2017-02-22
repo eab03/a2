d7daa9ad5ea3ba55841d0a72972eecbedbc93325
Liz Bright <elizabeth.anne.bright@gmail.com>
2017-02-22 08:09:55 -0500

    updated css and index ffor bootstrap

--- index.php
+++ index.php
@@ -11,45 +11,44 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 
-	<?php require('scrabbleTiles.php'); ?>
-
 	<title>Scrabble Word Score Calculator</title>
-
+	<?php require('scrabbleTiles.php'); ?>
 </head>
 
 <body>
 	<div class="wrapper">
 	<h1><span id="scrabble">SCRABBLE</span><br>WORD SCORE CALCULATOR</h1>
 
-	<div class="form-inputs">
-	<form method="GET" action="/" name="a2Form">
-		<div class="text-entry">
-			<label for="enterWord" id="textEntry">YOUR WORD:<br><span>Required (1-7 letters only please!)</span></label>
-			<input type="text" class="form-control" id="enterWord" name="enterWord" placeholder="Enter Your Word Here!" required="required" autofocus maxlength="7" value="<?=sanitize($enterWord)?>"><br>
-		</div><!--close div text-extry-->
+			<form method="GET" action="/" name="a2Form" class="form-horizontal">
+				<div class="form-group text-entry">
+					<label for="enterWord" class="control-label">YOUR WORD:</label>
+						<p class="form-control-static">Required (1-7 letters only please!)</p>
+					<input type="text" class="form-control" id="enterWord" name="enterWord" placeholder="Enter Your Word Here!" required="required" autofocus maxlength="7" value="<?=sanitize($enterWord)?>"><br>
+				</div><!--close div form-group-->
 
-		<fieldset id="radios">
-			<legend>BONUS POINTS</legend>
-				<label><input type="radio" name="bonus" value='none' <?php if($bonus == 'none') echo 'CHECKED'?>>None</label>
-				<label><input type="radio" name="bonus" value="double" <?php if($bonus == 'double') echo 'CHECKED'?>>Double word score</label>
-				<label><input type="radio" name="bonus" value="triple" <?php if($bonus == 'triple') echo 'CHECKED'?>>Triple word score</label>
-		</fieldset>
+				<div class="form-group radios">
+					<legend>BONUS POINTS</legend>
+							<label class="control-label"><input type="radio" class="form-check-input" name="bonus" value='none' <?php if($bonus == 'none') echo 'CHECKED'?>>&nbsp None</label><br>
+							<label class="control-label"><input type="radio" class="form-check-input" name="bonus" value="double" <?php if($bonus == 'double') echo 'CHECKED'?>>&nbsp Double word score</label><br>
+							<label class="control-label"><input type="radio" class="form-check-input" name="bonus" value="triple" <?php if($bonus == 'triple') echo 'CHECKED'?>>&nbsp Triple word score</label>
+				</div><!--close div form-group-->
 
-		<fieldset id="checkboxes">
-			<legend>INCLUDE 50 POINT "BINGO?"<br><span>(A word that uses all 7 letters!)</span></legend>
-				<label><input type='checkbox' name="extra" value="fifty" <?php if($extra == 'fifty') echo 'CHECKED'?>>Add 50 points!</label>
-		</fieldset>
-		<br>
-			<input type="submit" id="calculate_btn" name="calculate" value="calculate" class="btn btn-primary btn-small">
-			<input type="reset" id="reset-btn" name="reset" value="reset" class="btn btn-primary btn-small">
-	</form>
-</div> <!--close div form-->
+				<div class="form-group checkbox">
+					<legend>INCLUDE 50 POINT "BINGO?"</legend>
+						<p class="form-control-static">(A word that uses all 7 letters!)</p>
+						<label class="control-label"><input type='checkbox' class="form-check-input" name="extra" value="fifty" <?php if($extra == 'fifty') echo 'CHECKED'?>>&nbsp Add 50 points!</label>
+				</div><!--close div form-group-->
 
-	<div class="alert alert-success">
-	<p>Your word score is: <?=$sum?></p>
-	</div>
-</div> <!--close div wrapper-->
+				<br>
+				<input type="submit" class="btn btn-primary btn-small" id="calculate_btn" name="calculate" value="calculate">
+				<input type="reset" class="btn btn-primary btn-small" id="reset-btn" name="reset" value="reset">
+
+			</form><!--close form-->
+
+		<div class="alert alert-success">
+			<p>Your word score is: <?=$sum?></p>
+		</div> <!--close div alert-->
+	</div> <!--close div wrapper-->
 
 </body>
-
 </html>
